06ad78a45085a8e57d820852400646473996febb
John Hany <johnhany97@gmail.com>
2013-04-03 22:21:10 +0300

    Fix up PA for HDPI and please noobs, Add Maguro

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -2,6 +2,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/grouper.mk \
     $(LOCAL_DIR)/i9100.mk \
     $(LOCAL_DIR)/i9100g.mk \
+    $(LOCAL_DIR)/maguro.mk \
     $(LOCAL_DIR)/mako.mk 
 #    $(LOCAL_DIR)/i9300.mk \
 #    $(LOCAL_DIR)/n7000.mk \
---
+++ products/maguro.mk
@@ -0,0 +1,32 @@
+# Inherit AOSP device configuration for maguro.
+$(call inherit-product, device/samsung/maguro/full_maguro.mk)
+
+# Inherit GSM common stuff
+$(call inherit-product, vendor/revolt/configs/gsm.mk)
+
+# Inherit common bits
+$(call inherit-product, vendor/revolt/configs/common.mk)
+
+# Maguro Overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/revolt/overlay/maguro
+
+# PA OVERLAY_TARGET
+OVERLAY_TARGET := pa_xhdpi
+
+# Setup device specific product configuration.
+PRODUCT_NAME := revolt_maguro
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := maguro
+PRODUCT_MODEL := Galaxy Nexus
+PRODUCT_MANUFACTURER := samsung
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=yakju BUILD_FINGERPRINT=google/yakju/maguro:4.2.2/JDQ39/573038:user/release-keys PRIVATE_BUILD_DESC="yakju-user 4.2.2 JDQ39 573038 release-keys" BUILD_NUMBER=573038
+
+# Maguro specific packages
+PRODUCT_PACKAGES += \
+    Thinkfree
+
+# Copy Bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/revolt/prebuilt/bootanimation/xhdpi/bootanimation.zip:system/media/bootanimation.zip
