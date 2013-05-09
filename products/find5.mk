ec437111996b68c7027664840932a1dafad4954f
John Hany <johnhany97@gmail.com>
2013-05-09 12:58:44 +0300

    Overlays for Find5 + Use I9300 PA confs for Find5

--- products/find5.mk
+++ products/find5.mk
@@ -7,6 +7,9 @@ $(call inherit-product, vendor/revolt/configs/common.mk)
 # Inherit GSM common stuff
 $(call inherit-product, vendor/revolt/configs/gsm.mk)
 
+# Overlays
+PRODUCT_PACKAGE_OVERLAYS += vendor/revolt/overlay/find5 
+
 # Setup device specific product configuration
 PRODUCT_NAME := revolt_find5
 PRODUCT_BRAND := oppo
@@ -14,10 +17,11 @@ PRODUCT_DEVICE := find5
 PRODUCT_MODEL := Find5
 PRODUCT_MANUFACTURER := OPPO
 
-PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/mako:4.2/JOP40C/527662:user/release-keys PRIVATE_BUILD_DESC="occam-user 4.2 JOP40C 527662 release-keys" BUILD_NUMBER=527662
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT=OPPO/oppo_12069/FIND5:4.1.1/JRO03L/1362469752:user/release-keys PRIVATE_BUILD_DESC="msm8960-user 4.1.1 JRO03L eng.oppo.20130328.172033 release-keys"
 
+# Boot Animation
 PRODUCT_COPY_FILES += \
     vendor/revolt/prebuilt/bootanimation/xhdpi/bootanimation.zip:system/media/bootanimation.zip
 
 # PA OVERLAY_TARGET
-OVERLAY_TARGET := pa_xhdpi
+OVERLAY_TARGET := pa_i9300
