7cbd449d795b3b179305fc22e8f846214c8c770d
Hamzeh <hamzehalabdalla@gmail.com>
2013-05-25 22:43:09 -0500

    Added Support To Sprint S3

---
+++ products/d2spr.mk
@@ -0,0 +1,27 @@
+# Inherit AOSP device configuration for galaxys3.
+$(call inherit-product, device/samsung/d2spr/full_d2spr.mk)
+
+# Inherit AOKP common bits
+$(call inherit-product, vendor/revolt/configs/common.mk)
+
+# Inherit GSM common stuff
+$(call inherit-product, vendor/revolt/configs/gsm.mk)
+
+# Overlays
+PRODUCT_PACKAGE_OVERLAYS += vendor/revolt/overlay/d2spr
+
+# Setup device specific product configuration.
+PRODUCT_NAME := revolt_d2spr
+PRODUCT_BRAND := Samsung
+PRODUCT_DEVICE := d2spr
+PRODUCT_MODEL := SPH-L710
+PRODUCT_MANUFACTURER := Samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2spr TARGET_DEVICE=d2spr BUILD_FINGERPRINT="samsung/d2spr/d2spr:4.0.4/IMM76D/L710VPALG2:user/release-keys" PRIVATE_BUILD_DESC="d2spr-user 4.0.4 IMM76D L710VPALG2 release-keys"
+
+# Copy maguro specific prebuilt files
+PRODUCT_COPY_FILES += \
+    vendor/revolt/prebuilt/bootanimation/xhdpi/bootanimation.zip:system/media/bootanimation.zip
+
+# PA OVERLAY_TARGET
+OVERLAY_TARGET := pa_xhdpi
