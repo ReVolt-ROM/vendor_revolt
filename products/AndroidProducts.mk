4877e03c3f893235ca6c99b2440c23116b8df2d5
Hamzeh <hamzehalabdalla@gmail.com>
2013-05-25 22:44:19 -0500

    d2spr

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -20,5 +20,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/find5.mk \
     $(LOCAL_DIR)/n7100.mk \
     $(LOCAL_DIR)/m7.mk \
+    $(LOCAL_DIR)/d2spr.mk \
     $(LOCAL_DIR)/odin.mk \
     $(LOCAL_DIR)/yuga.mk
