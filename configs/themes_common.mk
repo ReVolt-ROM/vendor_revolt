a022ec81ce19552c038b20a6426adcac07cb0dba
John Hany <johnhany97@gmail.com>
2014-05-11 20:30:33 -0400

    New Theme Engine by CM

--- configs/themes_common.mk
+++ configs/themes_common.mk
@@ -1,8 +1,8 @@
-# T-Mobile theme engine
+# Theme engine
 PRODUCT_PACKAGES += \
-       ThemeManager \
-       ThemeChooser \
-       com.tmobile.themes 
+    ThemeChooser \
+    ThemesProvider
 
 PRODUCT_COPY_FILES += \
-       vendor/revolt/prebuilt/common/etc/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
+    vendor/revolt/prebuilt/common/etc/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
+    vendor/revolt/prebuilt/common/etc/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
