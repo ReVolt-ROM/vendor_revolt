d2391537708bb41644abae91e69df50f50f5c832
John Hany <johnhany97@gmail.com>
2014-08-29 00:40:51 +0300

    derp

--- products/deb.mk
+++ products/deb.mk
@@ -1,17 +1,19 @@
 # Inherit ReVolt common_tablet bits
-$(call inherit-product, vendor/aokp/configs/common_tablet.mk)
+$(call inherit-product, vendor/revolt/configs/common_tablet.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/asus/deb/full_deb.mk)
 
-# Device identifier. This must come after all inclusions
+# Setup device specific product configuration.
 PRODUCT_DEVICE := deb
 PRODUCT_NAME := revolt_deb
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := Asus
 
+# FingerPrint
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=razorg BUILD_FINGERPRINT=google/razorg/deb:4.4.2/KOT49H/937116:user/release-keys PRIVATE_BUILD_DESC="razorg-user 4.4.2 KOT49H 937116 release-keys"
 
+# Boot Animation
 PRODUCT_COPY_FILES += \
-    vendor/revolt/prebuilt/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+vendor/revolt/prebuilt/bootanimation/xhdpi/bootanimation.zip:system/media/bootanimation.zip
