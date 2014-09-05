a09a47cfc0c0422408da64b05d5af11d8c9225da
John Hany <johnhany97@gmail.com>
2014-09-05 12:09:13 +0300

    derp
    Signed-off-by: John Hany <johnhany97@gmail.com>

--- configs/vzw.mk
+++ configs/vzw.mk
@@ -3,20 +3,20 @@
 
 ifeq ($(VZW_UNIFIED),true)
 PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/vzw/app/VerizonSSO.apk:system/blobs/vzw/app/VerizonSSO.apk \
-    vendor/aokp/prebuilt/vzw/etc/permissions/com.vzw.hardware.ehrpd.xml:system/blobs/vzw/etc/permissions/com.vzw.hardware.ehrpd.xml \
-    vendor/aokp/prebuilt/vzw/etc/permissions/com.vzw.hardware.lte.xml:system/blobs/vzw/etc/permissions/com.vzw.hardware.lte.xml \
-    vendor/aokp/prebuilt/vzw/etc/permissions/com.vzw.vzwapnlib.xml:system/blobs/vzw/etc/permissions/com.vzw.vzwapnlib.xml \
-    vendor/aokp/prebuilt/vzw/lib/libmotricity.so:system/blobs/vzw/lib/libmotricity.so
+    vendor/revolt/prebuilt/vzw/app/VerizonSSO.apk:system/blobs/vzw/app/VerizonSSO.apk \
+    vendor/revolt/prebuilt/vzw/etc/permissions/com.vzw.hardware.ehrpd.xml:system/blobs/vzw/etc/permissions/com.vzw.hardware.ehrpd.xml \
+    vendor/revolt/prebuilt/vzw/etc/permissions/com.vzw.hardware.lte.xml:system/blobs/vzw/etc/permissions/com.vzw.hardware.lte.xml \
+    vendor/revolt/prebuilt/vzw/etc/permissions/com.vzw.vzwapnlib.xml:system/blobs/vzw/etc/permissions/com.vzw.vzwapnlib.xml \
+    vendor/revolt/prebuilt/vzw/lib/libmotricity.so:system/blobs/vzw/lib/libmotricity.so
 else
 PRODUCT_PACKAGES += \
     VerizonSSO
 
 PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/vzw/etc/permissions/com.vzw.hardware.ehrpd.xml:system/etc/permissions/com.vzw.hardware.ehrpd.xml \
-    vendor/aokp/prebuilt/vzw/etc/permissions/com.vzw.hardware.lte.xml:system/etc/permissions/com.vzw.hardware.lte.xml \
-    vendor/aokp/prebuilt/vzw/etc/permissions/com.vzw.vzwapnlib.xml:system/etc/permissions/com.vzw.vzwapnlib.xml \
-    vendor/aokp/prebuilt/vzw/lib/libmotricity.so:system/lib/libmotricity.so
+    vendor/revolt/prebuilt/vzw/etc/permissions/com.vzw.hardware.ehrpd.xml:system/etc/permissions/com.vzw.hardware.ehrpd.xml \
+    vendor/revolt/prebuilt/vzw/etc/permissions/com.vzw.hardware.lte.xml:system/etc/permissions/com.vzw.hardware.lte.xml \
+    vendor/revolt/prebuilt/vzw/etc/permissions/com.vzw.vzwapnlib.xml:system/etc/permissions/com.vzw.vzwapnlib.xml \
+    vendor/revolt/prebuilt/vzw/lib/libmotricity.so:system/lib/libmotricity.so
 
 PRODUCT_GMS_CLIENTID_BASE := android-verizon
 endif
